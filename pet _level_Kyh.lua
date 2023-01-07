a83e04545f16f5c86f3650466a9b0cdd56f479d9
Noisier <83883348+Noisier@users.noreply.github.com>
2023-01-07 08:13:41 -0300

    22.9.1

---
+++ pet _level_Kyh.lua
@@ -0,0 +1,6 @@
+gg.alert("script by kyh#8317 | Method by Michael1541#9506")
+gg.setRanges(gg.REGION_CODE_APP, gg.REGION_ANONYMOUS)
+gg.searchNumber("h F5 53 BE A9 F3 7B 01 A9 F4 5E 01 F0 35 3A 01 D0 88 3E 56 39 B5 56 41 F9 F3 03 00 AA 88 01 00 37 40 37 01 D0 00 D4 41 F9 DF C6 4B 97 80 3C 01 B0", gg.TYPE_BYTE)
+gg.getResults(gg.getResultCount())
+gg.editAll("h 00 00 80 D2 C0 03 5F D6 B4 5F 01 B0 B5 3A 01 F0 88 F6 7E 39 B5 46 44 F9 F3 03 00 AA 88 01 00 37 C0 37 01 F0 00 18 41 F9 CE AF 4A 97 80 37 01 F0", gg.TYPE_BYTE)
+gg.clearResults()
