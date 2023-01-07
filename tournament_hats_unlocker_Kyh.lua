9e174cbde4751c6f50faf2880be9ea778f0c1a33
Noisier <83883348+Noisier@users.noreply.github.com>
2023-01-07 08:45:44 -0300

    any version

---
+++ tournament_hats_unlocker_Kyh.lua
@@ -0,0 +1,58 @@
+function wait_for_action()
+    gg.setVisible(false)
+    while true do
+        if gg.isVisible() then
+            break
+        end
+		
+        gg.sleep(300)
+    end
+	
+    gg.setVisible(false)
+	gg.toast("Values Reversed, its now safe to buy.")
+end
+
+function floating_button()
+    gg.setVisible(false)
+    while true do
+        if gg.isVisible() then
+            break
+        end
+		
+        gg.sleep(300)
+    end
+	
+	gg.setVisible(false)
+	gg.setValues(revert)
+	gg.clearResults()	
+	gg.toast("Values Reversed, its now safe to buy.")
+end
+
+function unlock_wear_by_string(word)
+	gg.setVisible(false)
+	gg.clearResults()
+	gg.setRanges(gg.REGION_ANONYMOUS)
+	gg.searchNumber(";" .. word, gg.TYPE_WORD)
+	count = gg.getResultCount()
+	revert = gg.getResults(count)
+    gg.editAll(0, gg.TYPE_WORD)
+    gg.alert("Select item, then click the gameguardian icon.")
+    floating_button()
+end
+
+function tournament_hats_menu()
+	hats = {"tiara", "brain", "mushroom", "afro", "cowboy"}
+	menu = gg.choice({"Burning Tiara", "Evil Brain", "Mushroom Hat", "Afro", "Cowboy Hat"}, nil, "Select the hat you want.")
+	
+	i = 0
+	while not (i == #hats) do
+		i = i + 1
+		
+		if menu == i then
+			unlock_wear_by_string(hats[i])
+		end
+	end
+end
+
+gg.alert("script by kyh#8317 | Method by Michael1541#9506")
+tournament_hats_menu()
