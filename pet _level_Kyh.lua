17ec565660730cfa2e0ee8734dd0134eabc98613
Noisier <83883348+Noisier@users.noreply.github.com>
2023-01-09 18:53:51 -0300

    Update pet _level_Kyh.lua

--- pet _level_Kyh.lua
+++ pet _level_Kyh.lua
@@ -1,4 +1,4 @@
-gg.alert("script by kyh#8317 | Method by Michael1541#9506")
+gg.alert("script by kyh#8317 | method by Michael1541#9506")
 gg.setRanges(gg.REGION_CODE_APP, gg.REGION_ANONYMOUS)
 gg.searchNumber("h F5 53 BE A9 F3 7B 01 A9 F4 5E 01 F0 35 3A 01 D0 88 3E 56 39 B5 56 41 F9 F3 03 00 AA 88 01 00 37 40 37 01 D0 00 D4 41 F9 DF C6 4B 97 80 3C 01 B0", gg.TYPE_BYTE)
 gg.getResults(gg.getResultCount())
